88f8012e696b2eea6cf69d9d77e2fb2f1a938530
nebafyn-iq <nebiyu@fynz.app>
2026-01-23 16:24:57 +0300

    asynchronous messaging added

--- Transaction-service/cmd/main.go
+++ Transaction-service/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	//"strings"
 	"transaction/internal/account"
 	"transaction/internal/infrastructure/database"
+	"transaction/internal/infrastructure/kafka"
 	"transaction/internal/transaction"
 
 	//"transaction/Transaction-service/internal/infrastructure/database"
@@ -39,13 +42,32 @@ func main() {
 	transactionHandler := transaction.NewTransactionHandler(transactionService)
 
 	accountHandler := account.NewAccountHandler(accountRepo, transactionHandler.Balance)
-	
+
+	outboxRepo := transaction.NewPostgresOutboxRepository(db)
+	//publisher := transaction.NewLogPublisher()
+	producer := kafka.NewProducer([]string{"localhost:9092"})
+	worker := transaction.NewWorker(
+		outboxRepo,
+		producer,
+		"transaction.events",
+	)
+
+
 
 	router := httpinfra.NewRouter(
 		accountHandler.Routes(),
 		transactionHandler.Routes(),
 	)
 
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go worker.Start(ctx)
+
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+
+
+	
 }
